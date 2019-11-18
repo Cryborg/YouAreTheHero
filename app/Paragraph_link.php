1ef4a0b5adaadc77a33ef15c13ce485453ff0af0
Franck LECUVIER <franck.l@ocus.com>
2019-11-18 15:19:21 +0100

    Finished basic seeds

--- app/Paragraph_link.php
+++ app/Paragraph_link.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paragraph_link extends Model
 {
-    //
+    public $table = "paragraph_link";
+    public $timestamps = false;
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -46,57 +46,64 @@ class DatabaseSeeder extends Seeder {
             'created_at'    => now(),
         ]);
 
-        // First chapters
-        $p1 = Paragraph::create([
-            'title' => 'Paragraphe 1',
-            'description' => 'Dosis warp with friendship at the twisted ready room!<br>I raise this life, it\'s called galactic ellipse.',
-        ]);
-        $p2 = Paragraph::create([
-            'title' => 'Paragraphe 2',
-            'description' => 'Cum bulla unda, omnes abaculuses experientia noster, gratis musaes!<br>Rusticus, grandis tuss patienter imperium de superbus, fortis gemna.',
-        ]);
-        Paragraph_link::create([
-            'paragraph_from' => $p1,
-            'paragraph_to' => $p2,
-            'link_text' => 'Aller au chapitre 2',
-        ]);
+        foreach ([$storyMarty, $storyFred] as $story) {
+            // First chapters
+            $p1 = Paragraph::create([
+                'story_id' => $story->id,
+                'title' => 'Paragraphe 1',
+                'description' => 'Dosis warp with friendship at the twisted ready room!<br>I raise this life, it\'s called galactic ellipse.',
+            ]);
+            $p2 = Paragraph::create([
+                'story_id' => $story->id,
+                'title' => 'Paragraphe 2',
+                'description' => 'Cum bulla unda, omnes abaculuses experientia noster, gratis musaes!<br>Rusticus, grandis tuss patienter imperium de superbus, fortis gemna.',
+            ]);
+            Paragraph_link::create([
+                'paragraph_from' => $p1->id,
+                'paragraph_to' => $p2->id,
+                'link_text' => 'Aller au chapitre 2',
+            ]);
 
-        // Choices
-        $p3 = Paragraph::create([
-            'title' => 'Paragraphe 3',
-            'description' => 'Moons, explosion of the blessings, and strange individuals will always protect them!<br>To some, a thing is a peace for illuminating.',
-        ]);
-        $p4 = Paragraph::create([
-            'title' => 'Paragraphe 4',
-            'description' => 'The wind sails with courage, desire the captain\'s quarters before it hobbles!<br>Weird, warm anchors cruelly lead a gutless, salty whale.',
-        ]);
-        Paragraph_link::create([
-            'paragraph_from' => $p2,
-            'paragraph_to' => $p3,
-            'link_text' => 'Aller au chapitre 3',
-        ]);
-        Paragraph_link::create([
-            'paragraph_from' => $p2,
-            'paragraph_to' => $p4,
-            'link_text' => 'Aller au chapitre 4',
-        ]);
+            // Choices
+            $p3 = Paragraph::create([
+                'story_id' => $story->id,
+                'title' => 'Paragraphe 3',
+                'description' => 'Moons, explosion of the blessings, and strange individuals will always protect them!<br>To some, a thing is a peace for illuminating.',
+            ]);
+            $p4 = Paragraph::create([
+                'story_id' => $story->id,
+                'title' => 'Paragraphe 4',
+                'description' => 'The wind sails with courage, desire the captain\'s quarters before it hobbles!<br>Weird, warm anchors cruelly lead a gutless, salty whale.',
+            ]);
+            Paragraph_link::create([
+                'paragraph_from' => $p2->id,
+                'paragraph_to' => $p3->id,
+                'link_text' => 'Aller au chapitre 3',
+            ]);
+            Paragraph_link::create([
+                'paragraph_from' => $p2->id,
+                'paragraph_to' => $p4->id,
+                'link_text' => 'Aller au chapitre 4',
+            ]);
 
-        // Final chapter
-        $p5 = Paragraph::create([
-            'title' => 'Paragraphe 5',
-            'description' => 'The lieutenant commander is more particle now than planet. interstellar and wildly intelligent!<br>Fly without powerdrain, and we won’t love an astronaut.',
-            'is_last' => true,
-        ]);
-        Paragraph_link::create([
-            'paragraph_from' => $p3,
-            'paragraph_to' => $p5,
-            'link_text' => 'Aller au chapitre 3',
-        ]);
-        Paragraph_link::create([
-            'paragraph_from' => $p4,
-            'paragraph_to' => $p5,
-            'link_text' => 'Aller au chapitre 4',
-        ]);
+            // Final chapter
+            $p5 = Paragraph::create([
+                'story_id' => $story->id,
+                'title' => 'Paragraphe 5',
+                'description' => 'The lieutenant commander is more particle now than planet. interstellar and wildly intelligent!<br>Fly without powerdrain, and we won’t love an astronaut.',
+                'is_last' => true,
+            ]);
+            Paragraph_link::create([
+                'paragraph_from' => $p3->id,
+                'paragraph_to' => $p5->id,
+                'link_text' => 'Aller au chapitre 3',
+            ]);
+            Paragraph_link::create([
+                'paragraph_from' => $p4->id,
+                'paragraph_to' => $p5->id,
+                'link_text' => 'Aller au chapitre 4',
+            ]);
+        }
     }
 }
 
