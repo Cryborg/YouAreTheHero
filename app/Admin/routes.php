e1b8a23b296497a4db13049102d3476c20a2f1a0
Franck <cryborg@gmail.com>
2019-11-27 11:45:44 +0100

    Admin optimization

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,12 +11,13 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('admin.home');
-    $router->get('/list-stories', 'Stories\StoriesListController@list')->name('admin.stories.list');
-    $router->get('/list-stories/create', 'Stories\StoryCreateController@create')->name('admin.story.create');
+    $router->get('/stories', 'Stories\StoriesListController@list')->name('admin.stories.list');
+    $router->get('/stories/create', 'Stories\StoryCreateController@create')->name('admin.story.create');
     $router->post('/story/store', 'Stories\StoryCreateController@store')->name('admin.story.store');
-    $router->get('/list-pages', 'Pages\PagesListController@list')->name('admin.pages.list');
-    $router->get('/list-stories/{id}/edit', 'Stories\StoryEditController@edit')->name('admin.story.edit');
-    $router->put('/list-stories/{id}/update', 'Stories\StoryUpdateController@update')->name('admin.story.update');
+    $router->get('/stories/{story}/pages', 'Pages\PagesController@list')->name('admin.pages.list');
+    $router->get('/stories/{story}/pages/create', 'Pages\PagesController@create')->name('admin.pages.create');
+    $router->get('/stories/{id}/edit', 'Stories\StoryEditController@edit')->name('admin.story.edit');
+    $router->put('/stories/{id}/update', 'Stories\StoryUpdateController@update')->name('admin.story.update');
     $router->get('/page/get-form', 'Pages\PageFormController@form')->name('page.form');
     $router->post('/page/store', 'Pages\PageStoreController@store')->name('admin.page.store');
 });
