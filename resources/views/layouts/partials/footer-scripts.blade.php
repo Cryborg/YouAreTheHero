7527e72e1a28dd825c39d760991e97bef06cf5da
Franck <cryborg@gmail.com>
2020-10-14 12:21:34 +0000

    Testing Hotjar...

--- resources/views/layouts/partials/footer-scripts.blade.php
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -1,3 +1,16 @@
+<!-- Hotjar Tracking Code for https://www.youarethehero.fr -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2040780,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+<!-- End Hotjar -->
+
 <script src="{{ asset('js/d3.v5.min.js') }}" charset="utf-8"></script>
 <script src="{{ asset('js/graphlib-dot.js') }}"></script>
 <script src="{{ asset('js/dagre-d3.js') }}"></script>
