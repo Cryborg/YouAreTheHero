fdc782fcf8e4853a8e9d6f3cc974c1977194e919
Franck <franck.l@ocus.com>
2020-11-11 15:50:03 +0000

    Forgotten translations

--- resources/lang/en_GB/inbox.php
+++ resources/lang/en_GB/inbox.php
@@ -2,7 +2,12 @@
 
 return [
     'form' => [
+        'body' => 'Body',
+        'from' => 'From',
         'send' => 'Send',
+        'to' => 'To',
+
     ],
+    'new_message_label' => 'New message',
     'title' => 'Inbox',
 ];
--- resources/lang/fr_FR/inbox.php
+++ resources/lang/fr_FR/inbox.php
@@ -2,7 +2,11 @@
 
 return [
     'form' => [
+        'body' => 'Message',
+        'from' => 'De',
         'send' => 'Envoyer',
+        'to' => 'A',
     ],
+    'new_message_label' => 'Nouveau message',
     'title' => 'Messagerie',
 ];
